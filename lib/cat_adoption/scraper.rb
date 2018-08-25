fb086ae567a9d7da26df9d8a25a6df004de98f01
Laura <laurao148@gmail.com>
2018-08-25 19:24:43 +0000

    fix scrape_main_page method and add scrape_bio_page method

--- lib/cat_adoption/scraper.rb
+++ lib/cat_adoption/scraper.rb
@@ -8,9 +8,21 @@ class CatAdoption::Scraper
 # bio page
   # bio = doc.css("td.description-text")[0].text.strip
 
+
   def self.scrape_main_page
-   page = Nokogiri::HTML(open("http://www.austinhumanesociety.org/feline-friends/"))
-   each = doc.css(".pet").text
+  page = Nokogiri::HTML(open("http://www.austinhumanesociety.org/feline-friends/"))
+
+    page.css(".pet").each do |cat|
+      name = cat.css("h3").text.strip
+      url = cat.css("a").attr("href").text
+      CatAdoption::Cat.new(name, url)
+    end
   end
- end
+
+  def self.scrape_bio_page(cat)
+    page = cat.url
+    doc = Nokogiri::HTML(open(page))
+    cat.bio = doc.css("td.description-text")[0].text.strip
+    end
+  end
  
